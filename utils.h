6fc2a33ff7e75c7f1f32746df336d0d77cd673b5
Jackson <jacksonbnetherwoodimig@gmail.com>
2023-06-05 13:35:35 -0700

    1.0.1

--- utils.h
+++ utils.h
@@ -4,10 +4,10 @@
 uint16_t x_board = 0;
 uint16_t o_board = 0;
 
-void set_bit(uint16_t* board, int k) {
-    *board = (*board | (1 << k));
+void set_bit(uint16_t* board, int bit) {
+    *board = (*board | (1 << bit));
 }
 
-int get_bit(int n, int k) {
-    return ((n >> k) & 1);
+int get_bit(int board, int bit) {
+    return ((board >> bit) & 1);
 }
